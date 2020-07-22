27def70b943cbbb2ebc94eef6d7ed859651c607a
jhall9871 <james@jameshallmusic.com>
2020-07-22 15:31:08 -0500

    matches and connections work and display data

--- forte/src/Components/Routes/Connections/Connections.js
+++ forte/src/Components/Routes/Connections/Connections.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import ProfileCard from "../ProfileCard/ProfileCard";
+import apiUrl from "../../../apiConfig";
 
 export default function Connections() {
   // const dataContext = useContext(activeUser);
@@ -11,7 +12,7 @@ export default function Connections() {
     const makeAPICall = async () => {
       try {
         const response = await axios(
-          `http://localhost:4000/api/students/Dubrov`
+          `${apiUrl}/teachers/Ferguson`
         );
         setActiveUser(response.data[0]);
         if (response.data[0].teacher === true) {
@@ -35,30 +36,7 @@ export default function Connections() {
         <h1>
           {activeUser.firstName} {activeUser.lastName}'s Teachers
         </h1>
-        <ProfileCard />
-        {activeUser.myTeachers ? (
-          activeUser.myTeachers.map((teacher) => {
-            return (
-              <div className="connection-card">
-                <h3>
-                  {teacher.firstName} {teacher.lastName}
-                </h3>
-                <h4>Price/hr</h4>
-                <p>{teacher.rate} smackeroo's, y'all.</p>
-                <h4>Location</h4>
-                <p>{teacher.location}</p>
-                <h4>Instrument(s)/Discipline(s)</h4>
-                <ul>
-                  <li>{teacher.idiom1}</li>
-                  <li>{teacher.idiom2}</li>
-                  <li>{teacher.idiom3}</li>
-                </ul>
-              </div>
-            );
-          })
-        ) : (
-          <li>nothing yet</li>
-        )}
+        <ProfileCard activeUser={activeUser}/>
       </div>
     );
   } else {
--- forte/src/Components/Routes/Matches/Matches.js
+++ forte/src/Components/Routes/Matches/Matches.js
@@ -1,5 +1,110 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
+// import ProfileCard from "../ProfileCard/ProfileCard";
+import apiUrl from "../../../apiConfig";
 
 export default function Matches() {
-  return <div></div>;
+  // const dataContext = useContext(activeUser);
+  const [activeUser, setActiveUser] = useState({});
+  const [thisIsaTeacher, setThisIsaTeacher] = useState(false);
+  const [allTeachers, setAllTeachers] = useState([]);
+  const [allStudents, setAllStudents] = useState([]);
+
+  // make an array of all the teachers
+  useEffect(() => {
+    const makeAPICall = async () => {
+      try {
+        const response = await axios(`${apiUrl}/teachers`);
+        setAllTeachers(response.data);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+    makeAPICall();
+  }, []);
+
+  // make an array of all the students
+  useEffect(() => {
+    const makeAPICall = async () => {
+      try {
+        const response = await axios(`${apiUrl}/students`);
+        setAllStudents(response.data);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+    makeAPICall();
+  }, []);
+
+  // set the active user (eventually we'll get this from state)
+  useEffect(() => {
+    const makeAPICall = async () => {
+      try {
+        const response = await axios(`${apiUrl}/teachers/Silverfish`);
+        setActiveUser(response.data[0]);
+        if (response.data[0].teacher === true) {
+          setThisIsaTeacher(true);
+        }
+      } catch (err) {
+        console.error(err);
+      }
+    };
+    makeAPICall();
+  }, []);
+
+  if (activeUser === undefined) {
+    console.log("waiting for data");
+    return <p>No activeUser yet</p>;
+  } else if (thisIsaTeacher === false) {
+    // if a student, find all teachers who match instrument
+    let matches = allTeachers.filter((teacher) => {
+      return (
+        teacher.idiom1 === activeUser.idiom ||
+        teacher.idiom2 === activeUser.idiom ||
+        teacher.idiom3 === activeUser.idiom
+      );
+    });
+    console.log(matches);
+    return (
+      <div className="matches">
+        <h3>Student's Matches:</h3>
+        {matches.map((match) => {
+          let url = `/profile/${match.email}`;
+          return (
+            <Link to={url}>
+              <h4>
+                {match.firstName} {match.lastName}
+              </h4>
+            </Link>
+          );
+        })}
+      </div>
+    );
+  } else {
+        // if a teacher, find all students who match an idiom
+    let matches = allStudents.filter((student) => {
+      return (
+        activeUser.idiom1 === student.idiom ||
+        activeUser.idiom2 === student.idiom ||
+        activeUser.idiom3 === student.idiom
+      );
+    });
+    console.log(matches);
+    return (
+      <div className="matches">
+        <h3>Teacher's matches</h3>
+        {matches.map((match) => {
+          let url = `/profile/${match.email}`;
+          return (
+            <Link to={url}>
+              <h4>
+                {match.firstName} {match.lastName}
+              </h4>
+            </Link>
+          );
+        })}
+      </div>
+    );
+  }
 }
