b6fc61bd6d375e9fb5aed5f0401be6028e3c948a
jhall9871 <james@jameshallmusic.com>
2020-07-21 20:04:51 -0500

    works when hard-coded locally

--- forte/src/Components/Routes/Connections/Connections.js
+++ forte/src/Components/Routes/Connections/Connections.js
@@ -1,5 +1,50 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 export default function Connections() {
-  return <div></div>;
+  // const dataContext = useContext(activeUser);
+  const [activeUser, setActiveUser] = useState({});
+
+  useEffect(() => {
+    const makeAPICall = async () => {
+      try {
+        const response = await axios(
+          `http://localhost:4000/api/students/Robinson`
+        );
+        setActiveUser(response.data[0]);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+    makeAPICall();
+  }, []);
+
+  return (
+    <div className="connections">
+      <h1>Connections</h1>
+      {activeUser.myTeachers ? (
+        activeUser.myTeachers.map((teacher) => {
+          return (
+            <div className="connection-card">
+              <h3>
+                {teacher.firstName} {teacher.lastName}
+              </h3>
+              <h4>Price/hr</h4>
+              <p>{teacher.rate} smackeroo's, y'all.</p>
+              <h4>Location</h4>
+              <p>{teacher.location}</p>
+              <h4>Instrument(s)/Discipline(s)</h4>
+              <ul>
+                <li>{teacher.idiom1}</li>
+                <li>{teacher.idiom2}</li>
+                <li>{teacher.idiom3}</li>
+              </ul>
+            </div>
+          );
+        })
+      ) : (
+        <li>nothing yet</li>
+      )}
+    </div>
+  );
 }
