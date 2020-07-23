fb3d948aa1921bea35b6616fdbebaeba05a24726
Tyler Davis <tylerdavis@Tylers-iMac-2.local>
2020-07-23 08:39:18 -0400

    Created separate forms for Student/Teacher. Refactored Signup with new split forms.

--- forte/src/Components/Routes/SignUp/SignUp.js
+++ forte/src/Components/Routes/SignUp/SignUp.js
@@ -1,12 +1,11 @@
 import React, { useState } from "react";
-import { Redirect } from "react-router-dom";
+import TeacherForm from "../../Shared/TeacherForm/TeacherForm";
+import StudentForm from "../../Shared/StudentForm/StudentForm";
 import axios from "axios";
 import "./SignUp.css";
 
 export default function SignUp(props) {
   const [teacherStudent, setTeacherStudent] = useState("");
-  const [teacherSubmitted, setTeacherSubmitted] = useState(false);
-  const [studentSubmitted, setStudentSubmitted] = useState(false);
 
   const [teacherInput, setTeacherInput] = useState({
     firstName: "",
@@ -81,137 +80,29 @@ export default function SignUp(props) {
       .catch(console.error);
   };
 
-  // if (teacherSubmitted) {
-  //   return <Redirect to="/login" />;
-  // }
   if (teacherStudent === "Teacher") {
     return (
       <>
         <h3>Teacher Sign-Up</h3>
-        <form onSubmit={handleTeacherSubmit}>
-          <label>First Name:</label>
-          <input
-            onChange={handleTeacherChange}
-            name="firstName"
-            value={teacherInput.firstName}
-          />
-          <label>Last Name:</label>
-
-          <input
-            onChange={handleTeacherChange}
-            name="lastName"
-            value={teacherInput.lastName}
-          />
-          <label>Email:</label>
-          <input
-            onChange={handleTeacherChange}
-            name="email"
-            value={teacherInput.email}
-          />
-          <label>Primary Instrument:</label>
-
-          <input
-            onChange={handleTeacherChange}
-            name="idiom1"
-            value={teacherInput.idiom1}
-          />
-          <label>Secondary Instrument:</label>
-
-          <input
-            onChange={handleTeacherChange}
-            name="idiom2"
-            value={teacherInput.idiom2}
-          />
-          <label>Other Instrument:</label>
-
-          <input
-            onChange={handleTeacherChange}
-            name="idiom3"
-            value={teacherInput.idiom3}
-          />
-          <label>Language:</label>
-          <input
-            onChange={handleTeacherChange}
-            name="language"
-            value={teacherInput.language}
-          />
-          <label>Location (City, State):</label>
-
-          <input
-            onChange={handleTeacherChange}
-            name="location"
-            value={teacherInput.location}
-          />
-          <label>Rate/hr:</label>
-          <input
-            onChange={handleTeacherChange}
-            name="rate"
-            value={teacherInput.rate}
-          />
-          <label>Teaching Style:</label>
-          <input
-            onChange={handleTeacherChange}
-            name="teachingStyle"
-            value={teacherInput.teachingStyle}
-          />
-          <input type="submit" />
-        </form>
+        <TeacherForm
+          handleTeacherChange={handleTeacherChange}
+          handleTeacherSubmit={handleTeacherSubmit}
+          teacherInput={teacherInput}
+        />
         <button onClick={() => setTeacherStudent("")}>Back</button>
       </>
     );
   }
-  // if (studentSubmitted) {
-  //   return <Redirect to="/login" />;
-  // }
+
   if (teacherStudent === "Student") {
     return (
       <>
         <h3>Student Sign-Up</h3>
-        <form onSubmit={handleStudentSubmit}>
-          <label>First Name:</label>
-          <input
-            onChange={handleStudentChange}
-            name="firstName"
-            value={studentInput.firstName}
-          />
-          <label>Last Name:</label>
-          <input
-            onChange={handleStudentChange}
-            name="lastName"
-            value={studentInput.lastName}
-          />
-          <label>Email:</label>
-          <input
-            onChange={handleStudentChange}
-            name="email"
-            value={studentInput.email}
-          />
-          <label>Instrument:</label>
-          <input
-            onChange={handleStudentChange}
-            name="idiom"
-            value={studentInput.idiom}
-          />
-          <label>Language</label>
-          <input
-            onChange={handleStudentChange}
-            name="language"
-            value={studentInput.language}
-          />
-          <label>Location:</label>
-          <input
-            onChange={handleStudentChange}
-            name="location"
-            value={studentInput.location}
-          />
-          <label>Other:</label>
-          <input
-            onChange={handleStudentChange}
-            name="other"
-            value={studentInput.other}
-          />
-          <input type="submit" />
-        </form>
+        <StudentForm
+          handleStudentChange={handleStudentChange}
+          handleStudentSubmit={handleStudentSubmit}
+          studentInput={studentInput}
+        />
         <button onClick={() => setTeacherStudent("")}>Back</button>
       </>
     );
