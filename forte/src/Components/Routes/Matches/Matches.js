d8a478836c7e34f85694630a78c1f1135ab1bb19
Travis Tincher <travistincher@Traviss-MacBook-Air.local>
2020-07-23 22:21:35 -0500

    basic card style

--- forte/src/Components/Routes/Matches/Matches.js
+++ forte/src/Components/Routes/Matches/Matches.js
@@ -1,89 +1,94 @@
-import React, { useState, useEffect, useContext } from "react";
-import { DataContext } from "../../../App";
-import { Link } from "react-router-dom";
-import axios from "axios";
-import ProfileCard from "../ProfileCard/ProfileCard";
-import apiUrl from "../../../apiConfig";
+import React, { useState, useEffect, useContext } from 'react';
+import { DataContext } from '../../../App';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+import ProfileCard from '../ProfileCard/ProfileCard';
+import apiUrl from '../../../apiConfig';
+import './Matches.scss';
 
 export default function Matches() {
-  const dataContext = useContext(DataContext);
-  const activeUser = dataContext.activeUser[0];
-  const [allTeachers, setAllTeachers] = useState([]);
-  const [allStudents, setAllStudents] = useState([]);
+	const dataContext = useContext(DataContext);
+	const activeUser = dataContext.activeUser[0];
+	const [ allTeachers, setAllTeachers ] = useState([]);
+	const [ allStudents, setAllStudents ] = useState([]);
 
-  // make an array of all the teachers
-  useEffect(() => {
-    const makeAPICall = async () => {
-      try {
-        const response = await axios(`${apiUrl}/teachers`);
-        setAllTeachers(response.data);
-      } catch (err) {
-        console.error(err);
-      }
-    };
-    makeAPICall();
-  }, []);
+	// make an array of all the teachers
+	useEffect(() => {
+		const makeAPICall = async () => {
+			try {
+				const response = await axios(`${apiUrl}/teachers`);
+				setAllTeachers(response.data);
+			} catch (err) {
+				console.error(err);
+			}
+		};
+		makeAPICall();
+	}, []);
 
-  // make an array of all the students
-  useEffect(() => {
-    const makeAPICall = async () => {
-      try {
-        const response = await axios(`${apiUrl}/students`);
-        setAllStudents(response.data);
-      } catch (err) {
-        console.error(err);
-      }
-    };
-    makeAPICall();
-  }, []);
+	// make an array of all the students
+	useEffect(() => {
+		const makeAPICall = async () => {
+			try {
+				const response = await axios(`${apiUrl}/students`);
+				setAllStudents(response.data);
+			} catch (err) {
+				console.error(err);
+			}
+		};
+		makeAPICall();
+	}, []);
 
-  if (activeUser === undefined) {
-    console.log("waiting for data");
-    return <p>No activeUser yet</p>;
-  } else if (activeUser.teacher === false) {
-    // if a student, find all teachers who match instrument
-    let matches = allTeachers.filter((teacher) => {
-      return (
-        teacher.idiom1 === activeUser.idiom ||
-        teacher.idiom2 === activeUser.idiom ||
-        teacher.idiom3 === activeUser.idiom
-      );
-    });
-    console.log(matches);
-    return (
-      <div className="matches">
-        <h3>Student's Matches:</h3>
-        {matches.map((match) => {
-          let url = `/profile/${match.email}`;
-          return (
-            <Link key={match._id} to={url}>
-              <ProfileCard key={match._id} person={match} />
-            </Link>
-          );
-        })}
-      </div>
-    );
-  } else {
-    // if a teacher, find all students who match an idiom
-    let matches = allStudents.filter((student) => {
-      return (
-        activeUser.idiom1 === student.idiom ||
-        activeUser.idiom2 === student.idiom ||
-        activeUser.idiom3 === student.idiom
-      );
-    });
-    return (
-      <div className="matches">
-        <h3>Teacher's matches</h3>
-        {matches.map((match) => {
-          let url = `/profile/${match.email}`;
-          return (
-            <Link to={url}>
-              <ProfileCard key={match._id} person={match} />
-            </Link>
-          );
-        })}
-      </div>
-    );
-  }
+	if (activeUser === undefined) {
+		console.log('waiting for data');
+		return <p>No activeUser yet</p>;
+	} else if (activeUser.teacher === false) {
+		// if a student, find all teachers who match instrument
+		let matches = allTeachers.filter((teacher) => {
+			return (
+				teacher.idiom1 === activeUser.idiom ||
+				teacher.idiom2 === activeUser.idiom ||
+				teacher.idiom3 === activeUser.idiom
+			);
+		});
+		console.log(matches);
+		return (
+			<div className="matches">
+				<h3>Student's Matches:</h3>
+				<div className="cardContainer">
+					{matches.map((match) => {
+						let url = `/profile/${match.email}`;
+						return (
+							<Link key={match._id} to={url}>
+								<ProfileCard key={match._id} person={match} />
+							</Link>
+						);
+					})}
+				</div>
+			</div>
+		);
+	} else {
+		// if a teacher, find all students who match an idiom
+		let matches = allStudents.filter((student) => {
+			return (
+				activeUser.idiom1 === student.idiom ||
+				activeUser.idiom2 === student.idiom ||
+				activeUser.idiom3 === student.idiom
+			);
+		});
+		return (
+			<div className="matches">
+				<h3>Teacher's matches</h3>
+				<div className="cardContainer">
+					{matches.map((match) => {
+						let url = `/profile/${match.email}`;
+						return (
+							<Link to={url}>
+								<ProfileCard key={match._id} person={match} />
+							</Link>
+						);
+					})}
+				</div>
+			</div>
+		);
+	}
 }
--- forte/src/Components/Routes/ProfileCard/ProfileCard.js
+++ forte/src/Components/Routes/ProfileCard/ProfileCard.js
@@ -1,43 +1,42 @@
-import React from "react";
-
+import React from 'react';
+import './ProfileCard.scss';
 
 const ProfileCard = ({ person }) => {
-  if (person !== undefined && person.student === true) {
-    return (
-      <div className="connection-card">
-        <h4>
-          {person.firstName} {person.lastName}
-        </h4>
-        <h5>Location</h5>
-        <p>{person.location}</p>
-        <h5>Instrument</h5>
-        <p>{person.idiom}</p>
-        <h5>Other</h5>
-        <p>{person.other}</p>
-      </div>
-    );
-  } else if (person !== undefined && person.student === false) {
-    return (
-      <div className="connection-card">
-        <h4>
-          {person.firstName} {person.lastName}
-        </h4>
-        <h5>Location</h5>
-        <p>{person.location}</p>
-        <h5>Instrument(s)</h5>
-        <ul>
-          <li>{person.idiom1}</li>
-          <li>{person.idiom2}</li>
-          <li>{person.idiom3}</li>
-        </ul>
-        <h5>Teaching Style</h5>
-        <p>{person.teachingStyle}</p>
-
-      </div>
-    );
-  } else {
-    return <p></p>;
-  }
+	if (person !== undefined && person.student === true) {
+		return (
+			<div className="connection-card">
+				<h4>
+					{person.firstName} {person.lastName}
+				</h4>
+				<h5>Location</h5>
+				<p>{person.location}</p>
+				<h5>Instrument</h5>
+				<p>{person.idiom}</p>
+				<h5>Other</h5>
+				<p>{person.other}</p>
+			</div>
+		);
+	} else if (person !== undefined && person.student === false) {
+		return (
+			<div className="connection-card">
+				<h4>
+					{person.firstName} {person.lastName}
+				</h4>
+				<h5>Location</h5>
+				<p>{person.location}</p>
+				<h5>Instrument(s)</h5>
+				<ul>
+					<li>{person.idiom1}</li>
+					<li>{person.idiom2}</li>
+					<li>{person.idiom3}</li>
+				</ul>
+				<h5>Teaching Style</h5>
+				<p>{person.teachingStyle}</p>
+			</div>
+		);
+	} else {
+		return <p />;
+	}
 };
 
 export default ProfileCard;
